37bbad6e9dc975017ceadae7d267af95db16b2c5
c80678a <Totoro1@>
2020-09-13 00:51:00 -0300

    Revert "Primeiro commit kiki's delivery service!"
    This reverts commit fa5afe4b

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,10 +1,4 @@
 from src.models.base import DeclarativeBase
 from src.models.user import User
-from src.models.address import Address
-from src.models.delivery import Delivery
 
-__all__ = ["User", "DeclarativeBase", "Address", "Delivery"]
-
-"""
-Um usuário pode ter várias entregas vínculadas a ele
-"""
+__all__ = ["User", "DeclarativeBase"]
--- src/queries.py
+++ src/queries.py
@@ -1,36 +1,18 @@
-from typing import Dict, List
-
 from src.migrations.upgrade import upgrade
 from src.db_connection import DBConnector
-from src.models import Delivery, Address
 from src.models.user import User
 
 
-def create_user(user: Dict):
+def save_user():
     with DBConnector().conn_session() as session:
-        user_object = User(**user)
-        session.add(user_object)
+        user = User(name='John Snow', password='johnspassword')
+        session.add(user)
 
 
-def create_delivery(delivery: Dict):
+def get_user():
     with DBConnector().conn_session() as session:
-        delivery_object = Delivery(**delivery)
-        session.add(delivery_object)
+        db_user = session.query(User).filter_by(name='John Snow').first()
+        print(db_user)
 
 
-def create_address(address: Dict):
-    with DBConnector().conn_session() as session:
-        address_object = Address(**address)
-        session.add(address_object)
-        return address_object.id
 
-
-def get_user_by_email(email: str) -> Dict:
-    with DBConnector().conn_session() as session:
-        user = session.query(User).filter_by(email=email).first()
-        return user.to_dict()
-
-
-def get_all_deliveries() -> User:
-    with DBConnector().conn_session() as session:
-        return session.query(Delivery).all()
